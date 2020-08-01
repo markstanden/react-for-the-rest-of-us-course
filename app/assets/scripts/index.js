ed3d399041b4620586e1a6960a778a7e6175afaf
Mark Standen <markstanden80@icloud.com>
2020-08-01 07:31:52 +0100

    added useImmer functionality to save passing multiple unchanged object values into new objects at state change

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -20,6 +20,7 @@ import 'lazysizes'
 /*-------------------------------------------------------------------------------*/
 import React, { useState, useReducer } from 'react'
 import ReactDOM from 'react-dom'
+import { useImmerReducer } from 'use-immer'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Axios from 'axios'
 
@@ -49,23 +50,28 @@ function Main() {
   const initialState = {
     loggedIn: Boolean(localStorage.getItem('complexAppToken')),
     flashMessages: [],
+    user: {
+      token: '',
+      username: '',
+      avatar: '',
+    },
   }
 
   // CREATE REDUCER
-  const [state, dispatch] = useReducer(ourReducer, initialState)
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
   // DEFINE THE DISPATCH CALLS
-  function ourReducer(state, action) {
+  function ourReducer(draft, action) {
     switch (action.type) {
       case 'login':
-        return { loggedIn: true, flashMessages: state.flashMessages }
+        draft.loggedIn = true
+        break
       case 'logout':
-        return { loggedIn: false, flashMessages: state.flashMessages }
+        draft.loggedIn = false
+        break
       case 'flashMessage':
-        return {
-          loggedIn: state.loggedIn,
-          flashMessages: state.flashMessages.concat(action.value),
-        }
+        draft.flashMessages.push(action.value)
+        break
     }
   }
 
